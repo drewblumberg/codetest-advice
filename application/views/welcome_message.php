ddfb18ded801dfd1f6366e47318f3e7a4b82fb81
Drew Blumberg <alblumberg21@gmail.com>
2014-05-06 13:50:25 -0500

    Initial commit for codeigniter code test

---
+++ application/views/welcome_message.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Campaign Log</title>
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to the Campaign Log!</h1>
+
+	<div id="body">
+    <p>Here is the list of all campaigns!</p>
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
+
+</body>
+</html>
